bc7437c2b630006210de33ee58c28331f9d3074f
xtian08 <chriskint@yahoo.com>
2023-02-07 09:13:53 +0400

    Create generatejson.sh

---
+++ generatejson.sh
@@ -0,0 +1,19 @@
+python generatejson.py --base-url https://github.com --output ~/Desktop \
+--item \
+item-name='01-airwatch-23.01.0.pkg' \
+item-path='/Users/cdm436/Downloads/Extracted/osxbootstrap/OOBE-Files/setupassistant/01-airwatch-23.01.0.pkg' \
+item-stage='setupassistant' \
+item-type='package' \
+item-url='https://github.com/xtian08/NYU/blob/main/setupassistant/01-airwatch-21.01.0.pkg?raw=true' \
+script-do-not-wait=False \
+\
+--item \
+item-name='02-DEPNotify-1.1.6.pkg' \
+item-path='/Users/cdm436/Downloads/Extracted/osxbootstrap/OOBE-Files/setupassistant/02-DEPNotify-1.1.6.pkg' \
+item-stage='setupassistant' \
+item-type='package' \
+item-url='https://github.com/xtian08/NYU/blob/main/setupassistant/02-DEPNotify-1.1.6.pkg?raw=true' \
+script-do-not-wait=False \
+\
+
+exit
