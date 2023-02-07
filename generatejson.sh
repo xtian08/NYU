f5b154909db589608170b58a6bc4e4a107431af6
xtian08 <chriskint@yahoo.com>
2023-02-07 09:32:42 +0400

    Rename version

--- generatejson.sh
+++ generatejson.sh
@@ -1,10 +1,10 @@
-python generatejson.py --base-url https://github.com --output ~/Desktop \
+python generatejson.py \
 --item \
 item-name='01-airwatch-23.01.0.pkg' \
 item-path='/Users/cdm436/Downloads/Extracted/osxbootstrap/OOBE-Files/setupassistant/01-airwatch-23.01.0.pkg' \
 item-stage='setupassistant' \
 item-type='package' \
-item-url='https://github.com/xtian08/NYU/blob/main/setupassistant/01-airwatch-21.01.0.pkg?raw=true' \
+item-url='https://github.com/xtian08/NYU/blob/main/setupassistant/01-airwatch-21.01.0.pkg' \
 script-do-not-wait=False \
 \
 --item \
@@ -12,8 +12,12 @@ item-name='02-DEPNotify-1.1.6.pkg' \
 item-path='/Users/cdm436/Downloads/Extracted/osxbootstrap/OOBE-Files/setupassistant/02-DEPNotify-1.1.6.pkg' \
 item-stage='setupassistant' \
 item-type='package' \
-item-url='https://github.com/xtian08/NYU/blob/main/setupassistant/02-DEPNotify-1.1.6.pkg?raw=true' \
+item-url='https://github.com/xtian08/NYU/blob/main/setupassistant/02-DEPNotify-1.1.6.pkg' \
 script-do-not-wait=False \
 \
+\
+\
+--base-url https://github.com \
+--output ~/Desktop
 
 exit
